e3db5c8f4232d7100844bc3abb4020d0e369675d
Julien Veyssier <eneiluj@posteo.net>
2020-10-15 18:26:34 +0200

    add color and hide modules in webpack config
    Signed-off-by: Julien Veyssier <eneiluj@posteo.net>

--- webpack.js
+++ webpack.js
@@ -5,10 +5,15 @@ const buildMode = process.env.NODE_ENV
 const isDev = buildMode === 'development'
 webpackConfig.devtool = isDev ? 'cheap-source-map' : 'source-map'
 
+webpackConfig.stats = {
+    colors: true,
+    excludeModules: true,
+}
+
 webpackConfig.entry = {
-	personalSettings: { import: path.join(__dirname, 'src', 'personalSettings.js'), filename: 'integration_mastodon-personalSettings.js' },
-	dashboardHome: { import: path.join(__dirname, 'src', 'dashboardHome.js'), filename: 'integration_mastodon-dashboardHome.js' },
-	dashboard: { import: path.join(__dirname, 'src', 'dashboard.js'), filename: 'integration_mastodon-dashboard.js' },
+    personalSettings: { import: path.join(__dirname, 'src', 'personalSettings.js'), filename: 'integration_mastodon-personalSettings.js' },
+    dashboardHome: { import: path.join(__dirname, 'src', 'dashboardHome.js'), filename: 'integration_mastodon-dashboardHome.js' },
+    dashboard: { import: path.join(__dirname, 'src', 'dashboard.js'), filename: 'integration_mastodon-dashboard.js' },
 }
 
 module.exports = webpackConfig
