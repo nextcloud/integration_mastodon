5d69116cce63a394dd050f4bb58bb2fc3ce4738c
Julien Veyssier <julien-nc@posteo.net>
2023-01-29 22:09:00 +0100

    refs #41 fix target links for notifications as well
    Signed-off-by: Julien Veyssier <julien-nc@posteo.net>

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -227,14 +227,15 @@ export default {
 			}
 		},
 		filter(notifications) {
+			// return notifications.filter(n => n.type === 'favourite')
 			// no filtering for the moment
 			return notifications
 		},
 		getNotificationTarget(n) {
 			if (['favourite', 'mention', 'reblog'].includes(n.type)) {
-				return n.status.url
+				return this.mastodonUrl + '/@' + n.account?.acct + '/' + n.status?.id
 			} else if (['follow'].includes(n.type)) {
-				return n.account.url
+				return this.mastodonUrl + '/@' + n.account?.acct
 			} else if (['follow_request'].includes(n.type)) {
 				return this.mastodonUrl + '/web/follow_requests'
 			}
